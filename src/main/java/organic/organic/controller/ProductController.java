a5dc44c6c12517686a641be29232de80fc0dd6f3
Your Name <you@example.com>
2018-12-10 15:49:16 +0700

    firt

--- src/main/java/organic/organic/controller/ProductController.java
+++ src/main/java/organic/organic/controller/ProductController.java
@@ -12,7 +12,7 @@ import organic.organic.dao.product.ProductService;
 import organic.organic.model.product.Product;
 
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @Controller
 @RequestMapping("/api")
 public class ProductController {
