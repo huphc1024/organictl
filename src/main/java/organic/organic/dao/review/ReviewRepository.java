85a27a552574ac7d8a8766f7a2cbf0fbca607a5d
Your Name <you@example.com>
2018-12-26 12:16:45 +0700

    t

--- src/main/java/organic/organic/dao/review/ReviewRepository.java
+++ src/main/java/organic/organic/dao/review/ReviewRepository.java
@@ -13,10 +13,10 @@ public interface ReviewRepository  extends CrudRepository<Review, Integer> {
 	@Query(value = "SELECT id,comment,rating,tbl_product_id as rproduct ,tbl_user_id as ruser FROM reviews", nativeQuery = true)
 	List<Review> fillAllReview();
 	
-	@Query(value = "SELECT r.id, r.comment, r.rating , r.tbl_product_id as rproduct, r.tbl_user_id as ruser, u.email as remail FROM reviews r inner join tbl_product p on p.id = r.tbl_product_id inner join tbl_user u on u.id = r.tbl_user_id where r.tbl_product_id =?1", nativeQuery = true)
+	@Query(value = "SELECT r.id, r.comment, r.rating , r.tbl_product_id as rproduct, r.created_at, r.tbl_user_id as ruser, u.email as remail FROM reviews r inner join tbl_product p on p.id = r.tbl_product_id inner join tbl_user u on u.id = r.tbl_user_id where r.tbl_product_id =?1", nativeQuery = true)
 	List<Review> findByIdProductReview(int id);
 	
-	@Query(value = "SELECT r.id, r.comment, r.rating , r.tbl_product_id as rproduct , r.tbl_user_id as ruser FROM reviews r inner join tbl_product p on p.id = r.tbl_product_id inner join tbl_user u on u.id = r.tbl_user_id where r.tbl_user_id =?1", nativeQuery = true)
+	@Query(value = "SELECT r.id, r.comment, r.rating , r.tbl_product_id as rproduct ,r.created_at, r.tbl_user_id as ruser FROM reviews r inner join tbl_product p on p.id = r.tbl_product_id inner join tbl_user u on u.id = r.tbl_user_id where r.tbl_user_id =?1", nativeQuery = true)
 	List<Review> findByIdUsertReview(int id);
 	
 }
